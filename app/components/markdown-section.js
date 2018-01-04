376d601d855f09a2adca7d160bc6fe68f5584cec
@anulman <anulman@gmail.com>
2018-01-03 20:02:17 -0500

    fix(markdown-section): use config'ed namespace directly, vs appending a /

--- app/components/markdown-section.js
+++ app/components/markdown-section.js
@@ -2,5 +2,5 @@ import MarkdownSection from 'ember-cli-hyde/components/markdown-section';
 import ENV from '../config/environment';
 
 export default MarkdownSection.extend({
-  urlPrefix: `${ENV.hyde.namespace}/hyde`
+  urlPrefix: `${ENV.hyde.namespace || '/'}hyde`
 });
