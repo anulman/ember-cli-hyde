20e679558373839011778cfa3aed105dfa2654c1
@anulman <anulman@gmail.com>
2018-01-05 05:24:11 -0500

    refactor(config): extract config to its own lib file, + read from e-cli-build (vs config/env)

--- index.js
+++ index.js
@@ -6,21 +6,14 @@ const mergeTrees = require('broccoli-merge-trees');
 const Funnel = require('broccoli-funnel');
 const BroccoliHydeCompiler = require('broccoli-hyde-compiler').default;
 
+const hydeConfig = require('./lib/config');
 module.exports = {
   name: 'ember-cli-hyde',
 
-  config(env, baseConfig) {
-    this._directoryNames = readHydeDirectoryNames(baseConfig);
-
-    return {
-      hyde: {
-        namespace: ''
-      }
-    }
-  },
+  hydeConfig,
 
   treeForPublic(tree) {
-    let hydeTrees = this._directoryNames.map((name) => {
+    this.hydeTrees = this.hydeConfig().directories.map((name) => {
       let content = new Funnel(path.join(this.project.root, name), {
         include: ['**/*'],
         annotation: 'uncompiled Hyde files'
@@ -34,21 +27,7 @@ module.exports = {
     });
 
     return tree === undefined ?
-      mergeTrees(hydeTrees) :
-      mergeTrees(hydeTrees.concat(tree));
+      mergeTrees(this.hydeTrees) :
+      mergeTrees(this.hydeTrees.concat(tree));
   }
-}
-
-function readHydeDirectoryNames(config) {
-  if (config.hyde === undefined) {
-    return ['content'];
-  } else if (config.hyde.directories !== undefined) {
-    return config.hyde.directories instanceof Array ?
-      config.hyde.directories :
-      [config.hyde.directories];
-  } else {
-    return typeof config.hyde.directory === 'string' ?
-      [config.hyde.directory] :
-      ['content'];
-  }
-}
+};
