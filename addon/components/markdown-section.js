587cd66734d72ce2dff0916198d5598497daf7cd
@anulman <anulman@gmail.com>
2018-01-14 21:51:53 -0500

    refactor(markdown-section): update to consume markdown directly from item

--- addon/components/markdown-section.js
+++ addon/components/markdown-section.js
@@ -1,45 +1,12 @@
-import Ember from 'ember';
-import fetch from 'fetch';
+import Component from '@ember/component';
 
 import layout from '../templates/components/markdown-section';
-
-const {
-  Component,
-  ObjectProxy,
-  PromiseProxyMixin,
-  RSVP: { Promise },
-  computed,
-  isPresent
-} = Ember;
 
 const MarkdownSection = Component.extend({
   tagName: 'section',
   layout,
 
-  item: null,
-  urlPrefix: '/hyde',
-
-  markdown: computed('item.id', function() {
-    let ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
-
-    return ObjectPromiseProxy.create({
-      promise: fetch(`${this.get('urlPrefix')}/${this.get('item.id')}.md`)
-        .then((response) => response.blob())
-        .then((blob) => new Promise((resolve, reject) => {
-          let reader = new FileReader();
-
-          reader.onloadend = function didReadMarkdown() {
-            if (isPresent(reader.error)) {
-              reject(reader.error);
-            } else {
-              resolve(reader.result);
-            }
-          }
-
-          reader.readAsText(blob);
-        }))
-		});
-  })
+  item: null
 });
 
 MarkdownSection.reopenClass({
