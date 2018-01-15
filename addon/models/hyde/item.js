dfd6af190d64ee6d2a6b741c8085b47754d67602
@anulman <anulman@gmail.com>
2018-01-14 21:51:33 -0500

    refactor(item): update model to load markdown from hyde service

--- addon/models/hyde/item.js
+++ addon/models/hyde/item.js
@@ -1,10 +1,24 @@
 import DS from 'ember-data';
+import ObjectProxy from '@ember/object/proxy'
+import PromiseProxyMixin from '@ember/object/promise-proxy-mixin'
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
+const ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
 const { Model, attr, belongsTo, hasMany } = DS;
 
 export default Model.extend({
+  hyde: service(),
+
   yaml: attr(),
 
   parent: belongsTo('hyde/collection'),
-  tags: hasMany('hyde/collection')
+  tags: hasMany('hyde/collection'),
+
+  // todo: shoebox
+  markdown: computed('id', function() {
+    let promise = this.get('hyde').fetchFile(`${this.get('id')}.md`);
+
+    return ObjectPromiseProxy.create({ promise });
+  })
 });
